e81a84eeb3514d3a4ce84efae1ac65d7821bd90c
Ashik <kmdashik635@gmail.com>
2020-09-20 21:31:55 +0600

    files upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+jQuery(document).ready(function () {
+    jQuery('.slider_area').bxSlider();
+    jQuery('#menu').meanmenu({
+        meanMenuContainer: '#mobile_menu',
+        meanScreenWidth: 991,
+    })
+});
